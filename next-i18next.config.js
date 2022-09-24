57041609b1c827751ff6c2170b9c7f3692839aae
Enubia <marcoschuster@mailbox.org>
2022-09-24 17:16:56 +0200

    eslint fix

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,6 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'cro'],
-  },
+    locales: ['en', 'cro']
+  }
 };
