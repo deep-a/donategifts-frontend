403e290635fd7cf255e3126cbcc2b060626e7a98
Deep <pateldeep579@yahoo.com>
2022-09-27 20:02:58 -0500

    #63 - Rebuild UI for Contact Form. - Added translations. - Fixed ES Lint problems.

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import BaseLayout from '@components/layout/BaseLayout';
 import { useForm } from 'react-hook-form';
-import { contactResolver } from "@common/helper/FormHelper";
-import { Container, Form } from "react-bootstrap";
-import FormInput from "@components/authentication/FormInput";
-import Link from "next/link";
+import { contactResolver } from '@common/helper/FormHelper';
+import { Container, Form } from 'react-bootstrap';
+import FormInput from '@components/authentication/FormInput';
+import Link from 'next/link';
+import { useTranslation } from 'next-i18next';
 
 export async function getStaticProps({ locale }) {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ['common']))
+      ...(await serverSideTranslations(locale, ['common', 'contact']))
     }
   };
 }
@@ -18,75 +19,89 @@ export async function getStaticProps({ locale }) {
 export default function Contact() {
   const {
     register,
-    //handleSubmit,
+    // handleSubmit,
     formState: { errors }
   } = useForm({
     resolver: contactResolver
   });
 
+  const { t } = useTranslation('contact');
+
   return (
-    <BaseLayout pageTitle="Contact Donate Gifts">
+    <BaseLayout pageTitle={t('pageTitle')}>
       <section id="contact" className="center-elements mb-5">
         <div className="d-flex flex-wrap pt-5 pb-4">
           <Container className="row d-flex justify-content-center">
             <div className="col-11 text-center">
-              <h2 className="">Get in Touch</h2>
-              <p>Please fill out form and we will be in touch as soon as possible.
-                Perhaps your question has been answered, see our <Link href="/faq" className="link-faq">FAQ</Link> page.</p>
+              <h2>{t('section.heading')}</h2>
+              <p>
+                {t('section.instruction')}
+                <Link href="/faq" className="link-faq">
+                  {t('section.instruction2')}
+                </Link>
+                {t('section.instruction3')}
+              </p>
             </div>
             <div className="col-11 d-flex justify-content-center align-items-center">
               <div className="contact-form-wrapper">
                 <Form>
                   <div className=" col-md-12 d-flex flex-wrap justify-content-between">
                     <div className="col-md-6" id="contact-form-name">
-                      <label htmlFor="name" className="contact-label">Name</label>
+                      <label htmlFor="name" className="contact-label">
+                        {t('form.label.name')}
+                      </label>
                       <FormInput
                         type="text"
                         name="name"
                         registerFunc={register}
-                        placeholder="Name:"
+                        placeholder={t('form.placeholder.name')}
                         errorMsg={errors.fullName?.message}
                       />
                     </div>
                     <div className="col-md-6" id="contact-form-email">
-                        <label htmlFor="email" className="contact-label">Email</label>
-                        <FormInput
-                          id="email"
-                          containerClass="mt-5"
-                          type="email"
-                          name="email"
-                          registerFunc={register}
-                          placeholder="Email:"
-                          errorMsg={errors.email?.message}
-                        />
-                      </div>
+                      <label htmlFor="email" className="contact-label">
+                        {t('form.label.email')}
+                      </label>
+                      <FormInput
+                        id="email"
+                        containerClass="mt-5"
+                        type="email"
+                        name="email"
+                        registerFunc={register}
+                        placeholder={t('form.placeholder.email')}
+                        errorMsg={errors.email?.message}
+                      />
+                    </div>
                   </div>
                   <div className="mt-3">
-                    <Form.Label htmlFor="subject" className="contact-label">Subject</Form.Label>
+                    <Form.Label htmlFor="subject" className="contact-label">
+                      {t('form.label.subject')}
+                    </Form.Label>
                     <FormInput
                       id="subject"
                       containerClass="mt-3"
                       type="text"
                       name="subject"
                       registerFunc={register}
-                      placeholder="Subject:"
+                      placeholder={t('form.placeholder.subject')}
                       errorMsg={errors.subject?.message}
                     />
                   </div>
                   <div id="contact-form-message" className="p-3 form-group">
-                    <label htmlFor="message" className="contact-label">Message</label>
+                    <label htmlFor="message" className="contact-label">
+                      {t('form.label.message')}
+                    </label>
                     <textarea
                       rows="5"
                       id="message"
                       className="form-control contact-form-message mt-3"
                       {...register('message')}
-                      placeholder="Message:"
-                      errorMsg={errors.message?.message}
+                      placeholder={t('form.placeholder.message')}
                     />
                   </div>
                   <div className="center-elements pt-3">
                     <button type="submit" className="btn-navy-white-lg">
-                      Send Message
+                      {t('form.button')}
                     </button>
                   </div>
                 </Form>
